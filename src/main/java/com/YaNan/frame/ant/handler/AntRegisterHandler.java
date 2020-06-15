234cd6136e4772cb3ac47be79db8bfbf785f134a
tja414312570 <414312570@qq.com>
2020-06-15 19:05:12 +0800

    优化一些逻辑

--- src/main/java/com/YaNan/frame/ant/handler/AntRegisterHandler.java
+++ src/main/java/com/YaNan/frame/ant/handler/AntRegisterHandler.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Method;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.YaNan.frame.ant.AntContextConfigure;
 import com.YaNan.frame.ant.abstracts.AntAbstractRegisterHandler;
 import com.YaNan.frame.ant.exception.AntRegisterRuntimeException;
 import com.YaNan.frame.ant.interfaces.customer.AntClientService;
@@ -31,9 +30,7 @@ public class AntRegisterHandler extends AntAbstractRegisterHandler{
 	public void regist() {
 		try {
 			//组装注册信息
-			AntContextConfigure config = this.clientHandler.getRuntimeService().getContextConfigure();
 			AntRequest request = new AntRequest();
-			request.setService(config.getName());
 			request.setInvokeClass(AntClientService.class);
 			AntCustomer customer = new AntCustomer();
 			//设置自己的名字//如果名字为空。。。我也不知道填啥玩意
@@ -41,11 +38,12 @@ public class AntRegisterHandler extends AntAbstractRegisterHandler{
 			//设置附加数据
 			customer.setAttach("附加数据");
 			request.setInvokeParmeters(customer);
+			logger.debug("register info :"+customer);
 			Method method = ClassHelper.getClassHelper(AntClientService.class).getMethod("registClient", AntCustomer.class);
 			request.setInvokeMethod(method);
+			request.setTimeout(this.clientHandler.getRuntimeService().getContextConfigure().getTimeout());
 			RegisterResult result = (RegisterResult) clientHandler.getRuntimeService().request(clientHandler,request, true);
-			logger.debug("register info :"+customer);
-			logger.debug("Queen regist result:"+result);
+			logger.debug("regist result:"+result);
 			//从注册列表删除此注册
 			clientHandler.getRuntimeService().getAntRegisterServcie().removeRegister(this);
 			//判断是否注册成功
