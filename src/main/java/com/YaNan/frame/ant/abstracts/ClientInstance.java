4aaa69057ae4f3629fb05a7c1763bd5640b35f82
tja414312570 <414312570@qq.com>
2020-06-17 17:26:08 +0800

    添加注释

--- src/main/java/com/YaNan/frame/ant/abstracts/ClientInstance.java
+++ src/main/java/com/YaNan/frame/ant/abstracts/ClientInstance.java
@@ -6,18 +6,40 @@ import com.YaNan.frame.ant.interfaces.AntMessageSerialization;
 import com.YaNan.frame.ant.model.AntMessagePrototype;
 import com.YaNan.frame.ant.service.AntRuntimeService;
 
+/**
+ * 连接实例，将连接层抽象出来以实现多种协议
+ * @author yanan
+ */
 public interface ClientInstance {
-
+	/**
+	 * 写入消息
+	 * @param message
+	 */
 	void write(AntMessagePrototype message);
-
+	/**
+	 * 关闭链接
+	 * @param cause
+	 */
 	void close(Throwable cause) ;
-
+	/**
+	 * 获取服务
+	 * @return
+	 */
 	AntServiceInstance getServiceInstance();
-
+	/**
+	 * 获取地址
+	 * @return
+	 */
 	String getRemoteAddress();
-
+	/**
+	 * 获取消息序列化工具
+	 * @return
+	 */
 	AntMessageSerialization getSerailzationHandler();
-	
+	/**
+	 * 获取运行时
+	 * @return
+	 */
 	AntRuntimeService getRuntimeService();
 
 }
